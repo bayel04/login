8be932e1650d77f7038f696b8b67c303adca903b
bayel04 <93245123+bayel04@users.noreply.github.com>
2021-11-16 10:29:04 +0600

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ body {font-family: Arial, Helvetica, sans-serif;}
 
 
 /* Full-width input fields */
-input[type=text], input[type=password] {rustambek, ambal007
+input[type=text], input[type=password] {bayel,1234
     width: 100%;
     padding: 12px 20px;
     margin: 8px 0;
